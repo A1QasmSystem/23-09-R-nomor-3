b5a71e1b4b2799e949ad310d398d4b624fe4b97c
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-23 13:44:29 +0000

    Nomor 3

---
+++ index.js
@@ -0,0 +1,68 @@
+// // Nomor 1
+// const asalSantri = {
+//   Sidik: "Lampung",
+//   Wandi: "Prabumulih",
+//   Rahmad: "Medan",
+//   Usamah: "Ceribon",
+//   Syofyan: "Purwokerto",
+//   Aldi: "Kendari",
+//   Kardi: "Lombok",
+// };
+
+// let { Sidik, Wandi, Rahmad, Usamah, ...rest } = asalSantri;
+
+// console.log("Lampung", "Prabumulih", "Medan", "Ceribon", rest);
+// // =======================================================================
+
+// // Nomor 2
+
+// var kodeUnix = [
+//   23,
+//   89,
+//   67,
+//   29,
+//   192,
+//   6,
+//   2,
+//   129,
+//   21,
+//   872,
+//   891,
+//   901,
+//   70,
+//   61,
+//   78,
+//   62,
+//   32,
+//   90,
+//   90,
+// ];
+
+// console.log(
+//   kodeUnix
+//     .map(
+// console.log(nama);
+// console.log(status);
+// console.log(asal);
+// =====================================
+
+// Nomor 3
+function MentorProgrammer(nama, status, asal) {
+  nama();
+  status();
+  asal();
+}
+
+function nama() {
+  console.log("Nama Mentor Wandi Pratama");
+}
+
+function status() {
+  console.log("Status nya adalah Santri");
+}
+
+function asal() {
+  console.log("Berasal dari kota Prabumulih");
+}
+
+MentorProgrammer(nama, status, asal);
